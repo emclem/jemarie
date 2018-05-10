d700ffc5af927f41eef62f86b9da92966e09da14
emclem <ed_m_clemente@yahoo.com>
2018-05-11 05:57:44 +0800

    new template

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
-	// SCROLLED
+	// SCROLLED (HEADER)
 	$(window).scroll(function() {
-		$('.title-content').toggleClass('title-content-scrolled', $(this).scrollTop() > 0);
-		$('.header').toggleClass('header-scrolled', $(this).scrollTop() > 0);
+		$('.header').toggleClass('header-scrolled-first', $(this).scrollTop() > 0 && $(this).scrollTop() <= 149);
+		$('.header').toggleClass('header-scrolled-second', $(this).scrollTop() >= 150);
 	});
+
 
 
 	// RESPONSIVE MENU
@@ -16,11 +17,29 @@ $(document).ready(function() {
 	});
 
 
-	// BACK TO TOP
-	$('#totop').click(function() {
+
+
+
+	// SCROLL TO HOME (TOP)
+	$('#to-home, #to-home-header').click(function() {
 		$('html, body').animate({'scrollTop': '0px'}, 1000);
+		// change 'active' class
 		return false;
 	});
-
+	// SCROLL TO ABOUT
+	$("#to-about, #to-about-header, #to-about-arrow").click(function() {
+		$('html, body').animate({ scrollTop: $("#destination-about").offset().top }, 1000);
+		// change 'active' class
+		$('#to-about').addClass('active');
+		$('#to-about-header').addClass('active');
+		return false;
+	});
+	// SCROLL TO PORTFOLIO
+	$("#to-portfolio, #to-portfolio-header").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#destination-portfolio").offset().top
+		}, 1000);
+		return false;
+	});
 
 });
